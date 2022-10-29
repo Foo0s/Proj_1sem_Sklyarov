ac6e5cdbde6769faf9b8cac42824c591a1ba3943
SuperPuperKatushka <katrinsokolova05@gmail.com>
2022-10-29 19:21:50 +0300

    PZ_6

--- PZ_6/PZ_6_3.py
+++ PZ_6/PZ_6_3.py
@@ -1,11 +1,15 @@
 import random
 
-A = []
-B = [random.randint(1, 500)]
-B.append(random.randint(1, 500))
-i = 0
-while i <= random.randrange(1, 500):
-    A.append(random.randrange(1, 500))
-    i += 1
+c = int(input('Введите число: '))
+if c % 2 == 0:
+    A = [random.randint(0, 100) for a in range(c)]
+a, b =int(input('Введите x координату для точки: ')), int(input('Введите y координату для точки: '))
+B = []
+B.append(a)
+B.append(b)
+for n in A:
+    c = 1 / (A[n+2] - A[n])**2
+print(c)
 print(B)
 print(A)
+
