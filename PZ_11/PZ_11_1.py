97b031333071277dc69500f066493dc54b75058a
Foo0s <oraproavp@list.ru>
2023-01-15 00:45:41 +0300

    11_2

--- PZ_11/PZ_11_1.py
+++ PZ_11/PZ_11_1.py
@@ -8,6 +8,7 @@
     Отрицательные элементы:
     Количество отрицательных элементов:
     Среднее арифметическое:
+
     Содержимое второго файла:
     Положительные элементы:
     Количество положительных элементов:
@@ -20,17 +21,17 @@
 import random
 
 #Создание первого файла.
-f1 = open("file_1.txt", "w")
+f1 = open("file_1.txt", "w", encoding="UTF-8")
 list_1 = [random.randint(-10, 20) for i in range(random.randint(1, 20))]
 list_2 = [i for i in list_1 if i < 0]
 f1.write("Содержимое первого файла: \n")
 f1.writelines(f"Отрицательные элементы: {list_2}\n")
 f1.write(f"Количество отрицательных элементов: {len(list_2)}\n")
-f1.write(f"Среднее арифмитическое: {sum(list_2) / len(list_2)}")
+f1.write(f"Среднее арифмитическое: {sum(list_2) / len(list_2) if len(list_2) > 0 else 0}")
 f1.close()
 
 #Создание второго файла.
-f1 = open("file_2.txt", "w")
+f1 = open("file_2.txt", "w", encoding="UTF-8")
 list_1 = [random.randint(-10, 20) for i in range(random.randint(1, 20))]
 list_2 = [i for i in list_1 if i < 0] # Отрицательные элементы
 list_3 = [i for i in list_1 if i > 0] # Положительные элементы
--- PZ_11/PZ_11_2.py
+++ PZ_11/PZ_11_2.py
@@ -1,7 +1,14 @@
+"""21 Вариант. Скляров Владимир Дмитриевич.
+    Задача....
+    2. Из предложенного текстового файла (text18-21.txt) вывести на экран его содержимое,
+    количество знаков препинания. Сформировать новый файл, в который поместить текст в
+    стихотворной форме выведя строки в обратном порядке.
+"""
+
 #Открытие файла
 from typing import TextIO
 
-f1 = open("text18-21.txt", "r")
+f1 = open("text18-21.txt", "r", encoding="UTF-8")
 f2 = f1.read()
 score = 0 # счетчик
 for i in f2:
@@ -13,7 +20,18 @@ print(f"\n\nКоличество знаков препинаний: {score}\n")
 f1.close()
 
 #Создание нового файла.
-f4 = open("zadanie_2.txt", encoding='UTF-8')
-f2 = f4.readlines()
-for i in f2[::-1]:
+f4 = open("zadanie_2.txt", "r", encoding='UTF-8')
+f3 = reversed(f4.readlines())
+list_3 = []
+for i in f3:
+    list_3.append(i)
+f4.close()
+print(list_3)
+
+#Запись в файл данных.
+f4 = open("zadanie_2.txt", "w+", encoding="UTF-8")
+for i in list_3:
+    print(i)
     f4.write(i)
+list_3 = []
+f4.close()
